1a588b8f39404ae39660f1968e1a6a3f00e1fba9
ksenijasam <ksenija2702@gmail.com>
2023-03-08 17:17:16 +0100

    styling Movie component

--- src/pages/components/Movie.tsx
+++ src/pages/components/Movie.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import MovieModal from '../UI/MovieModal';
 
+import classes from './MovieList.module.css';
+
 interface movieDetails {
   title: string;
   moviePoster: string,
@@ -24,18 +26,20 @@ const Movie: React.FC<movieDetails> = (props: movieDetails) => {
   return (
     <div>
       {openModal && <MovieModal
-       title={props.title} 
-      moviePoster={props.moviePoster} 
-      year={props.year} plot={props.plot} 
-      genre={props.genre} 
-      closeModal={closeModal}
+        title={props.title}
+        moviePoster={props.moviePoster}
+        year={props.year} plot={props.plot}
+        genre={props.genre}
+        closeModal={closeModal}
       />}
-      <div onClick={openMovieDetailsModal}>
+      <div onClick={openMovieDetailsModal} className={classes.center}>
         <h2>{props.title}</h2>
-        <img src={props.moviePoster} width="300" height="400" alt="Movie Poster"></img>
-        <h2>{props.year}</h2>
-        <h2>{props.plot}</h2>
-        <h2>{props.genre}</h2>
+        <img src={props.moviePoster} width="100%%" height="400" alt="Movie Poster"></img>
+        <div>
+          <h2>Released in {props.year}</h2>
+          <section>{props.plot}</section>
+          <h2>{props.genre}</h2>
+        </div>
       </div>
     </div>
   );
