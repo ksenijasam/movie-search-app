dc914fde14fd98e0985e1fe00cf27f7a9513cd1f
ksenijasam <ksenija2702@gmail.com>
2023-03-09 19:12:51 +0100

    modal style

--- src/pages/UI/MovieModal.tsx
+++ src/pages/UI/MovieModal.tsx
@@ -7,27 +7,23 @@ const MovieModal: React.FC<any> = props => {
         <div>
             <div className={classes.backdrop} />
             <div className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
                 <div className={classes.content}>
+                    <header className={classes.header}>
+                        <h2>{props.title}</h2>
+                    </header>
                     <div>
                         <img src={props.moviePoster} width="300" height="400" alt="Movie image"></img>
                     </div>
-                    <div>
-                        {props.year}, {props.genre_id}
-                    </div>
-                    <div>
-                        <p>
-                            {props.plot}
-                        </p>
-                    </div>
+                    Released in {props.year}, genre {props.genre}
+                    <p>
+                        {props.plot}
+                    </p>
+                    <footer>
+                        <button type='button' onClick={props.closeModal} className={classes.button}>
+                            Close
+                        </button>
+                    </footer>
                 </div>
-                <footer>
-                    <div onClick={props.closeModal}>
-                        Close
-                    </div>
-                </footer>
             </div>
         </div>
     )
